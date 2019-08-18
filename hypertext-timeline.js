10affb62c1d7af4829c3b4c6aaa371df9be64103
Jakob Voss <voss@gbv.de>
2019-08-18 13:39:11 +0200

    Add short description of each diagram

--- hypertext-timeline.js
+++ hypertext-timeline.js
@@ -1,12 +1,20 @@
 $(function() {
   const queryBase = "//query.wikidata.org/"
-  const sparqlQueries = {}
+  const menu = {
+    "timeline": { sparql: null },
+    "traces": { sparql: null },
+    "people": { sparql: null }
+  }
+  var selectedMenuItem
 
   function selectMenu(name) {
+    selectedMenuItem = name
+
     $("#menu li").removeClass("active")
     $("#"+name).addClass("active")
 
-    const query = encodeURIComponent(sparqlQueries[name])
+    const query = encodeURIComponent(menu[name].sparql)
+    $("#description").html(menu[name].html)
     $("#iframes iframe").hide()
     var iframe = $("#iframe-"+name).show()
     if (!iframe.attr("src")) {
@@ -16,22 +24,32 @@ $(function() {
     $("#sparql").attr("href", queryBase + "#" + query)
   }
 
-  ;["timeline", "traces", "people"].forEach(function(name, index) {
+  Object.keys(menu).forEach(function(name) {
     var a = $('<a>').addClass("nav-link disabled")
     a.text(name.charAt(0).toUpperCase() + name.slice(1))
     $('<li class="nav-item">').attr('id', name).append(a).appendTo('#menu')
 
-    $.get(name +".sparql", function(sparql) {
-      sparqlQueries[name] = sparql
+    if ('sparql' in menu[name]) {
+      $.get(name +".sparql", function(sparql) {
+        menu[name].sparql = sparql
 
-      var iframe = $("<iframe>").hide()
-      iframe.attr("id","iframe-"+name)
-      iframe.appendTo($("#iframes"))
+        var iframe = $("<iframe>").hide()
+        iframe.attr("id","iframe-"+name)
+        iframe.appendTo($("#iframes"))
 
-      a.click(function(){ selectMenu(name) })
-      a.removeClass("disabled")
+        a.click(function(){ selectMenu(name) })
+        a.removeClass("disabled")
 
-      if(!index) selectMenu(name)
-    }, "text")
+        if (name == 'timeline') {
+          selectMenu(name)
+        }
+      }, "text")
+    }
+    $.get(name + ".html").done(function(html) {
+      menu[name].html = html
+      if (name == 'timeline') {
+        $("#description").html(html)
+      }
+    })
   })
 })
