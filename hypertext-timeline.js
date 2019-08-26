6aab2b21f5cbcc821a04c9f6c4a2033adf275f57
Jakob Voss <voss@gbv.de>
2019-08-26 21:52:51 +0200

    Add section with background information (#9)

--- hypertext-timeline.js
+++ hypertext-timeline.js
@@ -1,43 +1,57 @@
 $(function() {
   const queryBase = "//query.wikidata.org/"
+  const githubBase = "//github.com/nichtich/hypertext-timeline/blob/master/"
+
   const menu = {
-    "timeline": { sparql: null },
-    "traces": { sparql: null },
-    "people": { sparql: null }
+    "timeline": { sparql: null, html: null },
+    "traces": { sparql: null, html: null },
+    "people": { sparql: null, html: null },
+    "background": { html: null }
   }
   var selectedMenuItem
 
   function selectMenu(name) {
+    const item = menu[name]
     selectedMenuItem = name
 
     $("#menu li").removeClass("active")
     $("#"+name).addClass("active")
 
-    const query = encodeURIComponent(menu[name].sparql)
-    $("#description").html(menu[name].html)
-    $("#iframes iframe").hide()
-    var iframe = $("#iframe-"+name).show()
-    if (!iframe.attr("src")) {
-      iframe.attr("src", queryBase + "embed.html#" + query)
+    if (item.html) {
+      $("#description").show().html(item.html)
+      $("#html").attr("href", githubBase + name + ".html").show()
+    } else {
+      $("#description").hide()
+      $("#html").hide()
     }
 
-    $("#sparql").attr("href", queryBase + "#" + query)
+    $("#iframes iframe").hide()
+    $("#sparql").hide()
+    if (item.sparql) { 
+      const query = encodeURIComponent(item.sparql)
+      var iframe = $("#iframe-"+name).show()
+      if (!iframe.attr("src")) {
+        iframe.attr("src", queryBase + "embed.html#" + query)
+      }
+      $("#sparql").attr("href", queryBase + "#" + query).show()
+    }
   }
 
   Object.keys(menu).forEach(function(name) {
+    const item = menu[name]
+
     var a = $('<a>').addClass("nav-link disabled")
     a.text(name.charAt(0).toUpperCase() + name.slice(1))
     $('<li class="nav-item">').attr('id', name).append(a).appendTo('#menu')
+    a.click(function(){ selectMenu(name) })
 
-    if ('sparql' in menu[name]) {
+    if ('sparql' in item) {
       $.get(name +".sparql", function(sparql) {
-        menu[name].sparql = sparql
+        item.sparql = sparql
 
         var iframe = $("<iframe>").hide()
         iframe.attr("id","iframe-"+name)
         iframe.appendTo($("#iframes"))
-
-        a.click(function(){ selectMenu(name) })
         a.removeClass("disabled")
 
         if (name == 'timeline') {
@@ -45,11 +59,15 @@ $(function() {
         }
       }, "text")
     }
-    $.get(name + ".html").done(function(html) {
-      menu[name].html = html
-      if (name == 'timeline') {
-        $("#description").html(html)
-      }
-    })
+    if ('html' in item) {
+      $.get(name + ".html").done(function(html) {
+        item.html = html
+        if (name == 'timeline') {
+          $("#description").html(html).show()
+          $("#html").attr("href", githubBase + name + ".html").show()
+        }
+        a.removeClass("disabled")
+      })
+    }
   })
 })
