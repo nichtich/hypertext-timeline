86d4ec34cbf2a66f9c81665c2961633cb1baeee6
Jakob Voss <voss@gbv.de>
2019-09-17 10:23:25 +0200

    Fix flyer Makefile

--- flyer/screenshot.sh
+++ flyer/screenshot.sh
@@ -2,9 +2,14 @@
 
 SIZE=1280,800
 DELAY=2000
+CHROME=google-chrome
+
+if hash chromium-browser 2>/dev/null; then
+    CHROME=chromium-browser
+fi
 
 for NAME in "$@"; do
-  google-chrome --headless --run-all-compositor-stages-before-draw \
+    $CHROME --headless --run-all-compositor-stages-before-draw \
     --virtual-time-budget=$DELAY --window-size=$SIZE \
     --screenshot=$NAME.png "http://jakobvoss.de/hypertext-timeline/#$NAME"
 done
